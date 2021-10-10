324170368f078d1d927e8ca38a599148016144f6
rizkiakbar2302 <rizkiakbar2302@gmail.com>
2021-10-11 02:30:01 +0700

    Templating_4

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit ('no direct script access allowed');
 class Web extends CI_Controller{
     function __construct()
     {
-        parent::__construct();
+        parent::__construct('url');
     }
 
     public function index(){
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad Rizqi</li>
+        <li>Nama Belakang : Akbar Rabbani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl Raya Mardani</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>E-Sport</li>
+    </ul>
+</section>
