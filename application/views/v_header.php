f88cc6d216b7e7a140afaf1c6d08151013cbb962
rizkiakbar2302 <rizkiakbar2302@gmail.com>
2021-10-11 02:14:10 +0700

    Templating_1

---
+++ application/views/v_header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Prog II | Merancangn Template sederhana dengan codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan Codeigniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="<?php?>"></a>
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</body>
+
+</html>
