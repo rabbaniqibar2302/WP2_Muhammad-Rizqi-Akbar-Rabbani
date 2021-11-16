47951876ce246bf64000dcfb125fc78735659a51
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-11-16 19:49:33 +0700

    move file to templates

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -39,11 +39,11 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
